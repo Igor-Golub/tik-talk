64e0fa4a42a89d1dce6a1f8c37406b40500c4f5e
igorGolub <gitihonovich@gmail.com>
2024-12-30 13:01:55 +0300

    feat(user): Added user card.

---
+++ src/app/user/user-card/user-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-card',
+  imports: [],
+  templateUrl: './user-card.component.html',
+  styleUrl: './user-card.component.scss'
+})
+export class UserCardComponent {
+
+}
