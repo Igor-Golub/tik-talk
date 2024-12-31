cc9cff3b0348a80dfcc312a4484c1eda7868a5bc
igorGolub <gitihonovich@gmail.com>
2024-12-31 08:21:54 +0300

    feat(user): Added form handler.

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public form = new FormGroup({
+    login: new FormControl(''),
+    password: new FormControl('')
+  });
 
+  public onSubmit() {
+    console.log(this.form.value);
+  }
 }
