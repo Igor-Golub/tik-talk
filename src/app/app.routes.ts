841353c41f91f0b3affa53a07db629626aad92e4
igorGolub <gitihonovich@gmail.com>
2024-12-31 07:29:41 +0300

    feat(user): Added sidebar layout.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,31 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './ui/layout/layout.component';
 import { UsersComponent } from '../pages/users/users.component';
 import { LoginComponent } from '../pages/login/login.component';
+import { ProfileComponent } from '../pages/profile/profile.component';
 import { UsersSearchComponent } from '../pages/users-search/users-search.component';
 
 export const routes: Routes = [
   {
-    path: 'users',
-    component: UsersComponent,
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'users',
+        component: UsersComponent,
+      },
+      {
+        path: 'users-search',
+        component: UsersSearchComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      }
+    ]
   },
   {
     path: 'login',
     component: LoginComponent,
   },
-  {
-    path: 'users-search',
-    component: UsersSearchComponent,
-  }
 ];
