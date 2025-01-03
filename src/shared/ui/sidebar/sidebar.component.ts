ac035ae4766e9ee234fb5467c1c4ce39c8a547ac
igorGolub <gitihonovich@gmail.com>
2025-01-03 18:57:11 +0300

    feat(user): Added ui for nav list.

--- src/shared/ui/sidebar/sidebar.component.ts
+++ src/shared/ui/sidebar/sidebar.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { SvgComponent } from '../svg/svg.component';
+import { NgForOf } from '@angular/common';
+
+interface SidebarNavItem {
+  label: string;
+  icon: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [SvgComponent, NgForOf],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  public menuItemsConfig: SidebarNavItem[] = [
+    { label: 'Home', icon: 'home', link: '/' },
+    { label: 'Search', icon: 'search', link: '/' },
+    { label: 'Chat', icon: 'chat', link: '/' },
+  ];
 }
